63cd3dc35189e55ab7b4673f709b6dbfc885e87b
A24Nair <anand24nair@gmail.com>
2023-04-29 14:17:21 -0400

    Added overweight column Normalized data to have 0 as good and 1 as bad

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # Add 'overweight' column
-df['overweight'] = None
+df['overweight'] = np.where((df['weight']/((df['height']/100)**2))>25,1,0)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
-
+df['cholesterol'] = np.where(df['cholesterol'] == 1,0,1)
+df['gluc'] = np.where(df['gluc'] == 1,0,1)
 
 # Draw Categorical Plot
 def draw_cat_plot():
